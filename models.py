f0d12e31344c36fd79f52831cbe945190cdc44e6
狂拽酷炫 <nv@bestbuy.com>
2025-06-16 10:29:20 +0800

    first commit - Nothing done yet, just a framework, an empty one

---
+++ models.py
@@ -0,0 +1,44 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+# 时区设置
+def beijing_now():
+    return datetime.now(ZoneInfo("Asia/Shanghai"))
+
+
+class Tickets(Base):
+    __tablename__ = 'tickets'
+
+    id = Column(String, primary_key=True, nullable=False, unique=True)
+    title = Column(String, nullable=False)
+    creator = Column(String)
+    assignee = Column(String)
+    urgency = Column(String)
+    impact = Column(String)
+    priority = Column(String)
+    source = Column(String)
+    reporter = Column(String)
+    quantity = Column(Integer)
+    jira_link = Column(String)
+    org_1 = Column(String)
+    org_2 = Column(String)
+    org_3 = Column(String)
+    event_type_1 = Column(String)
+    event_type_2 = Column(String)
+    event_type_3 = Column(String)
+    reported_at = Column(DateTime(timezone=True), default=beijing_now)
+    completed_at = Column(DateTime(timezone=True), default=beijing_now)
+    completed = Column(Boolean, default=False)
+    escalated = Column(Boolean, default=False)
+    lead_time = Column(String)
+
+
+
+
+
+
+
+
+
